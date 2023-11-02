4d14f8f6c1563a574c39e5260f9e2fbb189cba7c
Joel Okhonjo <joelokhonjo3@example.com>
2023-11-02 11:30:16 +0300

    Applied modifications to files: 0x01-python-if_else_loops_functions/8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -7,4 +7,4 @@ def uppercase(s):
             uppercase_str += uppercase_char
         else:
             uppercase_str += char
-            print(uppercase_str)
+            print(f"{uppercase_str}")
