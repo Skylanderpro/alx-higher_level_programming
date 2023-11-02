ff23c1f84d76caf2c6e6199c4da5d2a6a6765a28
Joel Okhonjo <joelokhonjo3@example.com>
2023-11-02 19:52:39 +0300

    Applied modifications to files: 0x02-python-import_modules/4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import importlib.util
 
-spec = importlib.util.spec_from_file_location("hidden_4", "hidden_4.pyc")
-module = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(module)
+if __name__ == "__main__":
+    spec = importlib.util.spec_from_file_location("hidden_4", "hidden_4.pyc")
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
 
-for name in dir(module):
-    if not name.startswith("__"):
-        print(name)
+    for name in dir(module):
+        if not name.startswith("__"):
+            print(name)
