3b09399689701dc50985a94d25b28e1df330f36a
Joel Okhonjo <joelokhonjo3@example.com>
2023-11-02 11:28:45 +0300

    Applied modifications to files: 0x01-python-if_else_loops_functions/8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(s):
+    uppercase_str = ""
+    for char in s:
+        if 'a' <= char <= 'z':
+            uppercase_char = chr(ord(char) - 32)
+            uppercase_str += uppercase_char
+        else:
+            uppercase_str += char
+            print(uppercase_str)
