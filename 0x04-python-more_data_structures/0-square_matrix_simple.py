89dfda251ae0764646f8bc095f25c6fccc27dffc
Joel Okhonjo <joelokhonjo3@example.com>
2023-11-08 22:10:25 +0300

    Applied modifications to files: 0x04-python-more_data_structures/0-square_matrix_simple.py 0x04-python-more_data_structures/README.md 0x04-python-more_data_structures/p

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    squared_matrix = []
+
+    for row in matrix:
+        squared_row = []
+
+        for element n row:
+            squared_row.append(element ** 2)
+
+        squared_matrix.append(squared_row)
+
+    return squared_matrix
