45238bd138a9eb0c822f922ac09cfb7880cc69e6
Joel Okhonjo <joelokhonjo3@example.com>
2023-11-13 10:16:43 +0300

    Applied modifications to files: 0x03-python-data_structures/8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    """
+    Returns a tuple with the length of a string and its first character.
+    If the sentence is empty, the first character is set to None.
+
+    Args:
+    - sentence (str): The input string.
+
+    Returns:
+    - tuple: A tuple containing the length of the string and its first character.
+    """
+    if not sentence:
+        return (0, None)
+    else:
+        return (len(sentence), sentence[0])
