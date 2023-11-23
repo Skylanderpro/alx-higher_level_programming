f412a1840a7e06eee2a7af447e5e852f4d7b587b
Joel Okhonjo <joelokhonjo3@example.com>
2023-11-23 11:01:32 +0300

    Applied modifications to files: 0x05-python-exceptions/2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,13 +6,13 @@ def safe_print_list_integers(my_list=[], x=0):
         for i in my_list:
             if count < x:
                 try:
-                    print("{:d}".format(i), end="")
-                    count += 1
+                    if isinstance(i, int):
+                        print("{:d}".format(i))
+                        count += 1
                 except (ValueError, TypeError):
                     continue
             else:
                 break
-        print()
         return count
     except Exception as e:
         print("Traceback (most recent call last):")
