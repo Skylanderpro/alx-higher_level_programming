c411cdb26465ac65b0b2b2111cdf4b794db2d7c5
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-12-07 21:41:00 +0300

    Applied modifications to files: 0x06-python-classes/2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This code is for a class square
+"""
+
+
+class Square:
+    """
+    A square of private instance attribure: size
+    """
+    def __init__(self, size=0):
+        """
+        Initializes the Square instance.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
