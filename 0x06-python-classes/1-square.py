ce61e1d08734acd644131509d3d4635d1afdd86a
Joel Okhonjo <joelokhonjo3@gmail.com>
2023-12-07 21:24:35 +0300

    Applied modifications to files: 0x06-python-classes/1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This code is for a class square
+"""
+
+
+class Square:
+    """
+    A square of private instance attribure: size
+    """
+    def __init__(self, size):
+        """
+        Initializes the Square instance.
+        """
+        self.__size = size
